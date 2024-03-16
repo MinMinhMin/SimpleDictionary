f7324d9de46b368fdb59d5908568e4545a39ee9b
XyzHuy <149671889+XyzHuy@users.noreply.github.com>
2024-03-16 21:38:25 +0700

    first

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,20 +1,10 @@
 module com.front {
     requires javafx.controls;
     requires javafx.fxml;
-<<<<<<< HEAD
-    requires org.controlsfx.controls;
-    requires org.kordamp.bootstrapfx.core;
-	requires com.google.gson;
-
-	opens myapp to com.google.gson, javafx.fxml;
-
-	exports myapp;
-=======
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
 
     opens myapp to javafx.fxml;
     exports myapp;
->>>>>>> origin/main
 }
