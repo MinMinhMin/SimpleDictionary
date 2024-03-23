71f1cf2310b50301289785a8d3aba349b8acbce8
XyzHuy <149671889+XyzHuy@users.noreply.github.com>
2024-03-23 13:49:48 +0700

    update UI

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,11 @@
 module com.front {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+	requires com.google.gson;
 
-    opens myapp to javafx.fxml;
-    exports myapp;
+	opens myapp to com.google.gson, javafx.fxml;
+
+	exports myapp;
 }
--- src/main/java/myapp/ContextMenuController.java
+++ src/main/java/myapp/ContextMenuController.java
@@ -1,25 +1,82 @@
 package myapp;
 
+import java.io.InterruptedIOException;
+import javafx.concurrent.Task;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
 public class ContextMenuController {
+
 	private String selectedWord;
 
-	public void handleWordForm() {
-		handleOption("Word Form");
+	public void setStg() {
+		this.DetailBoxStage = new Stage();
 	}
 
-	public void handleOpposite() {
-		handleOption("Opposite");
+	private Stage DetailBoxStage = new Stage();
+
+	public void handleDetail() throws IOException {
+		handleOption("Word Detail");
 	}
 
-	public void handleClosestWord() {
-		handleOption("Closest Word");
+	public void handleAddGame() throws IOException {
+		handleOption("Add to game library");
 	}
 
-	private void handleOption(String option) {
-		System.out.println(selectedWord + ": " + option);
+	private void handleOption(String option) throws IOException {
+		if (option == "Word Detail") {
+			setStg();
+			showDetailBox(selectedWord);
+		}
+		System.out.print(option);
 	}
 
 	public void setSelectedWord(String word) {
 		this.selectedWord = word;
+	}
+
+	public void showDetailBox(String word) throws IOException{
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("DetailBox.fxml"));
+
+		Parent layout = loader.load();
+		DetailBoxController detailBoxController = loader.getController();
+		detailBoxController.setStage(DetailBoxStage);
+		DetailBoxStage.setTitle("Word Detail");
+		DetailBoxStage.initModality(Modality.APPLICATION_MODAL);
+		ProgressBar progressBar = new ProgressBar();
+		StackPane loadingPane = new StackPane();
+		loadingPane.getChildren().add(progressBar);
+		loadingPane.setStyle("-fx-border-color: rgb(7, 17, 17);-fx-border-width: 10;");
+		Scene loadingscene = new Scene(loadingPane,385,500);
+		DetailBoxStage.setX(115);
+		DetailBoxStage.setY(265);
+		DetailBoxStage.setResizable(false);
+		DetailBoxStage.initStyle(StageStyle.UNDECORATED);
+		DetailBoxStage.setScene(loadingscene);
+		DetailBoxStage.show();
+		//Loading....
+		Task<Scene> rederTask = new Task<Scene>() {
+			@Override
+			protected Scene call() throws Exception {
+				detailBoxController.setWord(selectedWord);
+				DetailBoxStage.setX(115);
+				DetailBoxStage.setY(265);
+				Scene scene = new Scene(layout, 385, 500);
+				return scene;
+			}
+		};
+		rederTask.setOnSucceeded(event ->DetailBoxStage.setScene(rederTask.getValue()));
+		new Thread(rederTask).start();
+
+
 	}
 }
